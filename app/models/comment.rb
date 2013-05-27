496dade8b0eb7f60d89a8579effa8ae9937153f8
Kevin F Browne <kbrowneatx@gmail.com>
2013-05-27 18:34:13 -0500

    adding nested attribs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ActiveRecord::Base
   attr_accessible :content, :post_id, :user_id
- 
   
   belongs_to :post
   belongs_to :user
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
-  attr_accessible :user_id, :content, :title
+  attr_accessible :user_id, :content, :title, :comments_attributes
   validates :title, presence: true, length: { maximum: 55 }
   validates :content, presence: true
   
   belongs_to :user
   has_many :comments, :dependent => :destroy
+  accepts_nested_attributes_for :comments
 end
